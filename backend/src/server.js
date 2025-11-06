2c763babb96ecd0a0ad579576ca8622d5feaa326
redondo <riad.reda.fethi@gmail.com>
2025-11-06 13:09:09 +0100

    Serveur : configuration app, base de données et serveur principal

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 
 import app from './app.js';
 import { sequelize, Film, Seance, Salle, Cinema } from './models/index.js';
+import { connectMongo } from './config/mongo.js';
 
 
 
@@ -11,12 +12,17 @@ const PORT = process.env.PORT || 3000;
 const startServer = async () => {
   try {
     await sequelize.authenticate();
-    console.log('✅ Connexion à la base de données réussie');
+    console.log(' Connexion à la base de données réussie');
   
     await sequelize.sync(); 
-    console.log('✅ Modèles synchronisés avec la base');
+    console.log('Modèles synchronisés avec la base');
+
+    //2- Connecter MongoDB
+    await connectMongo();
+    console.log(' Connexion à MongoDB réussie');
 
 
+    //Démarrer le serveur APRÈS que tout soit connecté
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (err) {
     console.error('Impossible de se connecter à la BDD:', err);
