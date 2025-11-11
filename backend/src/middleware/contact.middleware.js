fa75062591630ba0e38b228f1d6a16066e39e0cc
redondo <riad.reda.fethi@gmail.com>
2025-11-11 10:12:45 +0100

    📩 Mise à jour middleware de contact

--- backend/src/middleware/contact.middleware.js
+++ backend/src/middleware/contact.middleware.js
@@ -7,17 +7,26 @@ export const validateContact = [
     .optional()
     .isString().withMessage("Le nom doit être une chaîne de caractères.")
     .isLength({ max: 50 }).withMessage("Le nom ne doit pas dépasser 50 caractères.")
-    .customSanitizer(value => xss(value.trim())),
+     .customSanitizer(value => value ? xss(value.trim()) : ''),
+
+ // Vérifie et nettoie l'email
+  body('email')
+  .exists({ checkFalsy: true }).withMessage('L’email est obligatoire')
+    .isEmail().withMessage('Email invalide')
+    .bail()
+    .customSanitizer(value => (value ? value.trim().toLowerCase() : '')),
+
+
 
   body("titre")
     .notEmpty().withMessage("Le titre est obligatoire.")
     .isLength({ min: 5, max: 100 }).withMessage("Le titre doit contenir entre 5 et 100 caractères.")
-    .customSanitizer(value => xss(value.trim())),
+    .customSanitizer(value => value ? xss(value.trim()) : ''),
 
   body("description")
     .notEmpty().withMessage("La description est obligatoire.")
     .isLength({ min: 10, max: 1000 }).withMessage("La description doit contenir entre 10 et 1000 caractères.")
-    .customSanitizer(value => xss(value.trim())),
+  .customSanitizer(value => value ? xss(value.trim()) : ''),
 
   (req, res, next) => {
     const errors = validationResult(req);
