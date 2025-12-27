7e0e99d6bdb9e2c16fedda0534bae923b9fb7fdb
redondo <riad.reda.fethi@gmail.com>
2025-12-27 12:35:16 +0100

    ajout des fichiers dockerfile et dockerignore

--- backend/generateToken.js
+++ backend/generateToken.js
@@ -1,6 +1,6 @@
 // generateToken.js
 import jwt from "jsonwebtoken";
-
+import bcrypt from 'bcrypt';
 const payload = {
   id: 1,
   email: "admin@example.com",
@@ -10,3 +10,6 @@ const payload = {
 const token = jwt.sign(payload, "MaSuperCleSecrete123!", { expiresIn: "1d" });
 
 console.log("Token généré :", token);
+
+const hash = await bcrypt.hash('Client123!', 10);
+console.log(hash);
